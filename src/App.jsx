2466d9bf8bc4e66d94e13d8488bafa78a55a5f61
shen <santi.dionis@gmail.com>
2023-02-14 18:55:13 +0100

    ADD: react component

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css';
+function App(){
+    return(
+        <h2 className='title'>componente App</h2>
+    )
+
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 import './index.css';
+import App from './App'
 
 const rootElement = document.getElementById('root')
 
 
 const root = ReactDom.createRoot(document.getElementById('root'));
-root.render(<h1>HELLO world</h1>)
+root.render(
+    <App></App>
+);
