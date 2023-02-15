db5d161279a02af48f2c21164012a74d8cb9448c
shen <santi.dionis@gmail.com>
2023-02-15 21:10:45 +0100

    ADD: react Fracment

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
-import './App.css';
-import Testimonial from './components/Testimonials';
-function App(){
-    return(
-        <>
-        <h2 className='title'>componente App</h2>
-        <Testimonial></Testimonial>
-        <Testimonial></Testimonial>
-        <Testimonial></Testimonial>
-        </>
-    )
+import { Fragment } from "react";
+import "./App.css";
+import Testimonial from "./components/Testimonials";
+import photo1 from "./imagenes/profile1.jpg"
+import photo2 from "./imagenes/profile2.jpg"
+import photo3 from "./imagenes/profile3.jpg"
 
+function App() {
+  return (
+    <Fragment>
+      <Testimonial
+        img={photo1}
+        name="Anna Perez"
+        stars="⭐⭐⭐⭐"
+        text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+        esse, provident assumenda aliquid consequuntur totam, earum veniam quia
+        nesciunt rem sequi natus debitis cupiditate dolore, necessitatibus
+        maxime fuga dolorem repudiandae."
+      />
+       <Testimonial
+        img={photo2}
+        name="Juan perez"
+        stars="⭐⭐⭐"
+        text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+        esse, provident assumenda aliquid consequuntur totam, earum veniam quia
+        nesciunt rem sequi natus debitis cupiditate dolore, necessitatibus
+        maxime fuga dolorem repudiandae."
+      />
+       <Testimonial
+        img={photo3}
+        name="Alfonso Perex"
+        stars="⭐"
+        text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+        esse, provident assumenda aliquid consequuntur totam, earum veniam quia
+        nesciunt rem sequi natus debitis cupiditate dolore, necessitatibus
+        maxime fuga dolorem repudiandae."
+      />
+    </Fragment>
+  )
 }
 
-export default App
+export default App;
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,23 +1,20 @@
 import  './Testimonials.css';
-import photo1 from '../components/imagenes/profile2.jpg'
+import photo3 from '../imagenes/profile3.jpg'
 
-function Testimonial() {
+function Testimonial (props) {
   return (
     <div className="Testimonial__container">
       <div>
       <img 
-      src= {photo1} alt="" className="Testimonial__photo" />
+      src= {props.img} alt="photo profile" className="Testimonial__photo" ></img>
 
-      <h2 className="Testimonial__name">Juan Perez</h2>
+      <h2 className="Testimonial__name">{props.name}</h2>
       </div>
 
       <div className='Testimonial__content'>
-      <p className='Testimonial__stars'>⭐⭐⭐⭐⭐</p>
-      <p className='Testimonial__text'>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium
-        esse, provident assumenda aliquid consequuntur totam, earum veniam quia
-        nesciunt rem sequi natus debitis cupiditate dolore, necessitatibus
-        maxime fuga dolorem repudiandae.
+      <p className='Testimonial__stars'>{props.stars}</p>
+      <p className='Testimonial__text'>{props.text}
+      
       </p>
     </div>
     </div>
